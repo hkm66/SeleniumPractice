8176064f2375e398a24fed4c7b60b3e37e3c4d31
Yaseminc0 <yasemincalik77@gmail.com>
2023-12-21 14:18:40 +0000

    created test class and called methods

---
+++ src/test/java/com/group/tests/practice/weeklyCoding/Yasemin/Test.java
@@ -0,0 +1,12 @@
+package com.group.tests.practice.weeklyCoding.Yasemin;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("--------------------Unique Character---------------");
+        Week5_Task.findUniqueCharacter("AABBBCCCDDEFG");
+
+
+        System.out.println("--------------------String Reverse---------------");
+        Week5_Task.stringReverse("ABC");
+    }
+}
