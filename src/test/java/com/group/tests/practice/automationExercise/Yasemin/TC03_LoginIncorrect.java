de9b4865c6b15f831c76281b293fc4582502d5fc
Yaseminc0 <yasemincalik77@gmail.com>
2024-01-05 09:45:30 +0000

    T1,T2,T3 created and tested

---
+++ src/test/java/com/group/tests/practice/automationExercise/Yasemin/TC03_LoginIncorrect.java
@@ -0,0 +1,46 @@
+package com.group.tests.practice.automationExercise.Yasemin;
+
+import com.google.common.base.Verify;
+import com.group.tests.utilities.Driver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC03_LoginIncorrect {
+
+    AutomationRegisterPage registerPage;
+
+
+    @Test
+    public void loginInCorrectCrudental() {
+
+
+        registerPage = new AutomationRegisterPage();
+
+        //Test Case 2: Login User with correct email and password
+        //1. Launch browser
+        //2. Navigate to url 'http://automationexercise.com'
+        Driver.getDriver().get("http://automationexercise.com");
+        //3. Verify that home page is visible successfully
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), "https://automationexercise.com/");
+        System.out.println(Driver.getDriver().getCurrentUrl());
+
+        //4. Click on 'Signup / Login' button
+        registerPage.signUpLoginButton.click();
+
+        //5. Verify 'Login to your account' is visible
+        registerPage.loginAccountText.isDisplayed();
+
+        //6. Enter correct email address and password
+        registerPage.loginEmail.sendKeys("sevgi@abcd.com");
+        registerPage.loginPassword.sendKeys("123456");
+
+        //7. Click 'login' button
+        registerPage.loginButton.click();
+
+        //8. Verify error 'Your email or password is incorrect!' is visible
+        registerPage.incorrectText.isDisplayed();
+
+        Driver.closeDriver();
+    }
+
+    }
