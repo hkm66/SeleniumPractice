e52188ecf6bbf894a8b89fb4ae3e3a9e091b66ec
Roman <rbaidachenko@gmail.com>
2024-01-31 15:02:20 +0000

    week10 solved tasks

--- src/test/java/com/group/tests/Roman/Main.java
+++ src/test/java/com/group/tests/Roman/Main.java
@@ -2,28 +2,21 @@ package com.group.tests.Roman;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = {0, 0, 2, 0, 7, 2, 3};
-        int[] array2 = {1, 2, 0, 1, 0, 0, 6, 2};
-        System.out.println(Arrays.toString(Week9_hkm.concatArrays(array, array2)));
 
+        var str = "aabbbccddd";
+        System.out.println(Week10_hkm.freqOfEachElement(str));
 
+        var map = new HashMap<Integer, Integer>();
+        map.put(1, 16);
+        map.put(2, 9);
+        map.put(3, 12);
+        map.put(4, 6);
+        map.put(5, 11);
 
-        ArrayList<String> list = new ArrayList<>();
-        list.add("Ahmed");
-        list.add("John");
-        list.add("Eric");
-        list.add("Ahmed");
-
-        System.out.println(Week9_hkm.removeFromList(list, "Ahmed"));
-
-
-        ArrayList<Integer> numbers = new ArrayList<>();
-        for (int i = 95; i < 105; i++) {
-            numbers.add(i);
-        }
-        System.out.println(Week9_hkm.removeGreaterThan(numbers, 100));
+        System.out.println(Week10_hkm.minValueFromMap(map));
     }
 }
